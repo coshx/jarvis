a687340ed4f6bf50987e03604c0b8969752a5a53
Calvin Delamere <calvin.delamere@gmail.com>
2013-07-24 15:53:56 -0600

    fix root view

--- jarvis/jarvis/urls.py
+++ jarvis/jarvis/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'jarvis.views.home', name='home'),
+    url(r'^$', 'chrome.views.home', name='home')
     # url(r'^jarvis/', include('jarvis.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
