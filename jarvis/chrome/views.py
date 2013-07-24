2444bf59b55ee60c3a523ce8e58c4e7d0481df24
Calvin Delamere <calvin.delamere@gmail.com>
2013-07-24 17:16:16 -0600

    sample json response

--- jarvis/chrome/views.py
+++ jarvis/chrome/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.utils import simplejson
 
 def home(request):
-    template = loader.get_template('chrome/index.html')
-    context = RequestContext(request)
-    return HttpResponse(template.render(context))
+  template = loader.get_template('chrome/index.html')
+  context = RequestContext(request)
+  return HttpResponse(template.render(context))
+
+def process_command(request):
+  # lots of real logic needed here. Stuff is going to need to be extracted from here eventually...
+  response_data = {'response': 'It is 88 degrees outside'}
+  return HttpResponse(simplejson.dumps(response_data), content_type="application/json") 
--- jarvis/jarvis/urls.py
+++ jarvis/jarvis/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'chrome.views.home', name='home')
+    url(r'^$', 'chrome.views.home', name='home'),
+    url(r'^process_command/', 'chrome.views.process_command', name='process_command')
     # url(r'^jarvis/', include('jarvis.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
