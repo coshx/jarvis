427726b025b815ebe57c1ec01e07c28c2cfca7cd
Calvin Delamere <calvin.delamere@gmail.com>
2013-07-25 15:23:40 -0600

    some terrible nl processing

--- jarvis/chrome/models.py
+++ jarvis/chrome/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+import nltk
 
-# Create your models here.
+class CommandProcessor(models.Model):
+  
+  @staticmethod
+  def extractCommand(command):
+    # Jarvis/Travis what is the weather
+    if not CommandProcessor.isCommand(command):
+      return False
+    command = command.split(' ',1)[1] #removes jarvis from the command
+    
+    entities = CommandProcessor.entities(command)
+    for entity in entities:
+      if entity[0] == "weather":
+        return "weather"
+
+    return "unknown"
+  
+  @staticmethod
+  def isCommand(command):
+    if command.split(" ")[0].lower() == "travis" or command.split(" ")[0].lower() == "jarvis":
+      return True
+    else:
+      return False
+
+  @staticmethod
+  def entities(command):
+    tokens = nltk.word_tokenize(command)
+    tagged = nltk.pos_tag(tokens)
+    return nltk.chunk.ne_chunk(tagged)
+    
+
--- jarvis/chrome/views.py
+++ jarvis/chrome/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.utils import simplejson
 import json, urllib2
+from chrome.models import CommandProcessor
 
 def home(request):
   template = loader.get_template('chrome/index.html')
@@ -15,11 +16,11 @@ def textToSpeechTest(request):
 
 def process_command(request):
     # lots of real logic needed here. Stuff is going to need to be extracted from here eventually...
-    requested_service = request.GET['q']
-    if requested_service == "weather":
+    command = CommandProcessor.extractCommand(request.GET['q'])
+    if command == "weather":
         weatherData = urllib2.urlopen("http://api.openweathermap.org/data/2.5/find?q=Boulder,co&units=imperial").read()
         weatherData_temp = json.loads(weatherData)['list'][0]['main']['temp']
-        response_data = {'response': 'It is ' + str(weatherData_temp) + ' degrees outside'}
+        response_data = {'speak': 'It is ' + str(weatherData_temp) + ' degrees outside'}
     else:
-        response_data = {'response': request.GET}
+        response_data = {'speak': 'That command is not yet avaliable or was stated in a way I do not understand'}
     return HttpResponse(simplejson.dumps(response_data), content_type="application/json") 
