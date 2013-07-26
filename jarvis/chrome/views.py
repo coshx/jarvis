faf7a25e2832b3ecf312ca87886cf00ddaff59be
Evan Derby <somekidwithhtml@gmail.com>
2013-07-26 09:47:59 -0700

    Finished msuic implementation

--- jarvis/chrome/views.py
+++ jarvis/chrome/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.utils import simplejson
-import json, urllib2, urllib
+import json, urllib2
 from chrome.models import CommandProcessor
 
 def home(request):
@@ -28,7 +28,7 @@ def process_command(request):
         weatherData = urllib2.urlopen("http://api.openweathermap.org/data/2.5/find?q=Boulder,co&units=imperial").read()
         weatherData_temp = json.loads(weatherData)['list'][0]['main']['temp']
         response_data = {'speak': 'It is ' + str(weatherData_temp) + ' degrees outside'}
-    elif extractedCommand["command"] == "play":
+    elif extractedCommand['command'] == "play":
         youtubeData = urllib2.urlopen('https://www.googleapis.com/youtube/v3/search?q='+extractedCommand['data']+'&key=AIzaSyBL6aPKYByygs9oHB5rStYhTBKtklqRkrI&part=snippet').read()
         youtubeData_url = json.loads(youtubeData)['items'][0]['id']['videoId'];
         response_data = {'action': 'youtube', 'data': youtubeData_url}
