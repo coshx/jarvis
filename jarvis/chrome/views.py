eac023f217095dcaba1b3856518b395a61255a56
Evan Derby <somekidwithhtml@gmail.com>
2013-07-26 09:27:54 -0700

    Added music path to views

--- jarvis/chrome/views.py
+++ jarvis/chrome/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.utils import simplejson
-import json, urllib2
+import json, urllib2, urllib
 from chrome.models import CommandProcessor
 
 def home(request):
@@ -28,6 +28,10 @@ def process_command(request):
         weatherData = urllib2.urlopen("http://api.openweathermap.org/data/2.5/find?q=Boulder,co&units=imperial").read()
         weatherData_temp = json.loads(weatherData)['list'][0]['main']['temp']
         response_data = {'speak': 'It is ' + str(weatherData_temp) + ' degrees outside'}
+    elif extractedCommand["command"] == "play":
+        youtubeData = urllib2.urlopen('https://www.googleapis.com/youtube/v3/search?q='+extractedCommand['data']+'&key=AIzaSyBL6aPKYByygs9oHB5rStYhTBKtklqRkrI&part=snippet').read()
+        youtubeData_url = json.loads(youtubeData)['items'][0]['id']['videoId'];
+        response_data = {'youtube':youtubeData_url}
     else:
         response_data = {'speak': 'That command is not yet avaliable or was stated in a way I do not understand'}
     return HttpResponse(simplejson.dumps(response_data), content_type="application/json") 
