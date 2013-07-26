c05c74b9b8cdd2f0a04192ecf452aaff1680a23c
Calvin Delamere <calvin.delamere@gmail.com>
2013-07-26 14:38:00 -0600

    moving things around for heroku

--- jarvis/jarvis/settings.py
+++ jarvis/jarvis/settings.py
@@ -65,20 +65,20 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+#STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 # Additional locations of static files
-print os.path.abspath(os.path.join(ABSOLUTE_PROJECT_ROOT, 'staticfiles/'))
-STATICFILES_DIRS = (
-    os.path.abspath(os.path.join(ABSOLUTE_PROJECT_ROOT, 'staticfiles/')),
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+# print os.path.abspath(os.path.join(ABSOLUTE_PROJECT_ROOT, 'staticfiles/'))
+# STATICFILES_DIRS = (
+#     os.path.abspath(os.path.join(ABSOLUTE_PROJECT_ROOT, 'staticfiles/')),
+#     # Put strings here, like "/home/html/static" or "C:/www/django/static".
+#     # Always use forward slashes, even on Windows.
+#     # Don't forget to use absolute paths, not relative paths.
+# )
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -164,3 +164,16 @@ LOGGING = {
         },
     }
 }
+
+# Heroku Static asset configuration
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+print "---HEROKU-----"
+print BASE_DIR
+print os.path.join(BASE_DIR, 'static')
+print "---------"
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
