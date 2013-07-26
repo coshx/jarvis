958851c45e8171699f5bb4a15c863214c4e6d001
Calvin Delamere <calvin.delamere@gmail.com>
2013-07-26 11:26:55 -0600

    accept popeyes, for pause... why not...

--- jarvis/chrome/models.py
+++ jarvis/chrome/models.py
@@ -19,7 +19,7 @@ class CommandProcessor(models.Model):
       entities.pop(0) #remove play
       return {'command': 'play', 'data': urllib.quote(' '.join(entities))}
 
-    if entities[0] == "pause" or entities[0] == "paws":
+    if entities[0] == "pause" or entities[0] == "paws" or entities[0] == "popeyes":
       return {'command': 'pause'}
 
     #check for weather
